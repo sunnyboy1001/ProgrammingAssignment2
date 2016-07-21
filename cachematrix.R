518b4b52487651d0bdf68c6a793ba978b0f18731
Sunny Funng <sunnyboy1001@vip.qq.com>
2016-07-22 03:16:41 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,39 +1,32 @@
+## Put comments here that give an overall description of what your
+## functions do
 
-## The function makeCacheMatrix creates a special matrix,
-## which is really a list containing a function to
+## Write a short comment describing this function
 
-## 1. set the value of the matrix
-## 2. get the value of the matrix
-## 3. set the value of the inverse
-## 4. get the value of the inverse
-
-makeCacheMatrix <- function(x = matrix()){
-    i <- NULL
-    set <- function(y){
-        x <<- y
-        i <<- NULL
-    }
-    get <- function() x
-    setinverse <- function(inverse) i <<- inverse
-    getinverse <- function() i
-    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+makeCacheMatrix <- function(x = matrix()) {
+	m <- NULL
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(solve) m <<- solve
+	getinverse <- function() m
+	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## The following function calculates the inverse of the matrix created by the 
-## above function. However, it first checks to see whether the inverse has been
-## calculated already. If so, it gets the inverse from cache and skips the 
-## computation. Otherwise, it calculates the inverse of the matrix and sets the 
-## cache via the setinverse function.
+## Write a short comment describing this function
 
-cacheSolve <- function(x, ...){
-    i <- x$getinverse()
-    if(!is.null(i)){
-        message("getting cached data")
-        return(i)
-    }
-    data <- x$get()
-    i <- solve(data, ...)
-    x$setinverse(i)
-    i
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+	m <- x$getinverse()
+	if(!is.null(m)) {
+		message(“getting cached data”)
+		return(m)
+	}
+	data <- x$get()
+	m <- solve(data, …)
+	x$setinverse(m)
+	m
 }
